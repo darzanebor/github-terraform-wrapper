5d46583a0068ab30655d4e0d8b17f8ae8d797e01
Denis G <51837169+darzanebor@users.noreply.github.com>
2022-11-29 15:23:46 +0300

    Update tf.sh

--- tf.sh
+++ tf.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
+# Define some variables
 HELP_MESSAGE="sub commands:\n  - fmt\n  - install\n  - init\n  - plan\n  - apply"
-TERRAFORM_URL="https://releases.hashicorp.com/terraform/$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')/terraform_$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')_linux_amd64.zip"
+
+# Get latest terraform version and build url if custom version is not set.
+if [ -n "$TERRAFORM_VERSION" ]; then
+  TERRAFORM_URL="https://releases.hashicorp.com/terraform/$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')/terraform_$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')_linux_amd64.zip"
+else
+  TERRAFORM_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+fi
+
+# Wrap around terraform
 case $1 in
   "install" )
     if [ ! -f "${HOME}/.local/bin/terraform" ]; then
+        echo -e "Downloading terraform"
         mkdir -p "${HOME}/.local/bin"
         echo "${HOME}/.local/bin" >> $GITHUB_PATH
-        echo -e "Downloading terraform"
         curl -L "${TERRAFORM_URL}" -o terraform.zip
         unzip terraform.zip
         mv terraform "${HOME}/.local/bin/"
-        echo -e "Installed terraform:\n"
         rm -rf {terraform.zip,terraform}
+        echo -e "Installed terraform:\n"        
         terraform version
     fi
     ;;
