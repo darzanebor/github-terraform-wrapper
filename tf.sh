4486fded5c1ee165e6e58dd4e849fa0e147b91ea
Denis G <51837169+darzanebor@users.noreply.github.com>
2022-11-29 13:38:48 +0300

    Update tf.sh

--- tf.sh
+++ tf.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 HELP_MESSAGE="sub commands:\n  - fmt"
 case $1 in
+  "init" )
+    terraform -chdir=$2 init -input=false
+    ;;
+  "plan" )
+    terraform -chdir=$2 plan -input=false
+    ;;
+  "apply" )
+    terraform -chdir=$2 apply -auto-approve
+    ;;    
   "fmt" )
-    terraform fmt -recursive $2
+    terraform -chdir=$2 fmt -recursive
     ;;
   * )
     echo -e "${HELP_MESSAGE}\n"
